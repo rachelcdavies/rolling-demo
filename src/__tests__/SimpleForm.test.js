0780fad1b6dbe8904832da153441314afe298cf5
rachelcdavies <rachelcdavies@users.noreply.github.com>
2019-06-09 19:27:52 +0100

    Rename test folder.

--- src/__tests__/SimpleForm.test.js
+++ src/__tests__/SimpleForm.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../App';
+import SimpleForm from '../SimpleForm';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<SimpleForm />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
