72cc9bd25a2ef303f631a13d5ee331fc576a2f3d
rachelcdavies <rachelcdavies@users.noreply.github.com>
2019-06-14 23:44:56 +0100

    Expand mappings to include label for select type.

--- src/mappings.js
+++ src/mappings.js
@@ -1,18 +1,30 @@
 import React from 'react';
 
 export  const mappings = {
-  string: ({name, label, key, onChange, onBlur, ...additionalProps}) => (
-    <label className="Form__label">{label}
-    <input name={name} key={key} onChange={onChange} onBlur={onBlur} {...additionalProps} className="Form__string-field" />
+  string: ({name, label, key, onChange, onBlur, ...additionalProps}) =>
+      (<label className="Form__label">{label}
+        <input name={name} key={key} onChange={onChange} onBlur={onBlur} {...additionalProps} className="Form__string-field" />
     </label>),
   boolean: ({ name, label, key }) =>
-    (<label className="Form__label">
-      {label}
+    (<label className="Form__label">{label}
       <input name={name} key={key} type="checkbox" />
     </label>),
   number: ({ name, label, key }) =>
-    (<label className="Form__label">
-      {label}
+    (<label className="Form__label">{label}
       <input name={name} key={key} type="number" />
     </label>),
+    select: ({name, label, key, options, onChange, onBlur, ...additionalProps}) =>
+        (<label className="Form__label">
+            {label}
+            <select name={name} key={key} onChange={onChange} onBlur={onBlur} {...additionalProps}>
+            {options.map(
+                ({ value, text, ...optionProps }) => (
+                    <option key={`${value}${text}`} value={value} {...optionProps}>
+                        {text || value}
+                    </option>
+                ),
+            )
+            }
+            </select>
+        </label>),
 };
