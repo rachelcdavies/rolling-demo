02ba25adf821df9a5a0b6075bde25038e84ce706
rachelcdavies <rachelcdavies@users.noreply.github.com>
2019-06-14 23:43:30 +0100

    Add tests.

--- src/__tests__/SimpleForm.test.js
+++ src/__tests__/SimpleForm.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
 import SimpleForm from '../SimpleForm';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SimpleForm />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  ReactDOM.unmountComponentAtNode(container);
+  document.body.removeChild(container);
+  container = null;
+});
+
+describe('Simple Form', () => {
+  it('renders no drink selected', () => {
+    act(() => {
+      ReactDOM.render(<SimpleForm />, container);
+    });
+    const responseMessage = (container.querySelector('p'));
+    expect(responseMessage.textContent).toBe('No drink selected');
+  });
+  it('submit with default input', () => {
+    act(() => {
+      ReactDOM.render(<SimpleForm />, container);
+    });
+    const submitButton = (container.querySelector('button'));
+    act(() => {
+      submitButton.dispatchEvent(new MouseEvent('click'));
+    });
+    const responseMessage = (container.querySelector('p'));
+
+    expect(responseMessage.textContent).toBe('Preparing water for you');
+  });
 });
