a11728ddea94f2c306e8b1a83996f5e54d66fc24
rachelcdavies <rachel.davies@tesglobal.com>
2019-08-08 17:37:38 +0100

    Fix to prevent warning: React does not recognize the `setFieldValue` prop on a DOM element.

--- src/mappings.js
+++ src/mappings.js
@@ -1,30 +1,29 @@
 import React from 'react';
 
 export  const mappings = {
-  string: ({name, label, key, onChange, onBlur, ...additionalProps}) =>
+  string: ({name, label, key, onChange, onBlur, setFieldValue, ...additionalProps}) =>
       (<label className="Form__label">{label}
         <input name={name} key={key} onChange={onChange} onBlur={onBlur} {...additionalProps} className="Form__string-field" />
     </label>),
-  boolean: ({ name, label, key }) =>
+  number: ({ name, label, key, onChange, onBlur, setFieldValue, ...additionalProps}) =>
     (<label className="Form__label">{label}
-      <input name={name} key={key} type="checkbox" />
+      <input name={name} key={key} type="number" onChange={onChange} onBlur={onBlur} {...additionalProps} />
     </label>),
-  number: ({ name, label, key }) =>
+  boolean: ({ name, label, key, onChange, onBlur, setFieldValue, ...additionalProps }) =>
     (<label className="Form__label">{label}
-      <input name={name} key={key} type="number" />
+      <input name={name} key={key} type="checkbox" onChange={onChange} onBlur={onBlur} {...additionalProps}/>
     </label>),
-    select: ({name, label, key, options, onChange, onBlur, ...additionalProps}) =>
-        (<label className="Form__label">
-            {label}
-            <select name={name} key={key} onChange={onChange} onBlur={onBlur} {...additionalProps}>
-            {options.map(
-                ({ value, text, ...optionProps }) => (
-                    <option key={`${value}${text}`} value={value} {...optionProps}>
-                        {text || value}
-                    </option>
-                ),
-            )
-            }
-            </select>
-        </label>),
+  select: ({name, label, key, options, onChange, onBlur, setFieldValue, ...additionalProps}) =>
+    (<label className="Form__label">
+      {label}
+      <select name={name} key={key} onChange={onChange} onBlur={onBlur} {...additionalProps}>
+        {options.map(
+          ({ value, text, ...optionProps }) => (
+            <option key={`${value}${text}`} value={value} {...optionProps}>
+              {text || value}
+            </option>
+          ),
+        )}
+      </select>
+    </label>),
 };
