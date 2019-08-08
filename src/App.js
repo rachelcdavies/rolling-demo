1d9c7fe81f5b27b177959d378b76e262d2c7e8b2
rachelcdavies <rachel.davies@tesglobal.com>
2019-08-08 16:21:17 +0100

    Minor refactoring.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/App.css';
-import SimpleForm from './SimpleForm';
+import { SimpleForm } from './SimpleForm';
 
 
 function App() {
--- src/SimpleForm.jsx
+++ src/SimpleForm.jsx
@@ -6,29 +6,24 @@ import { mappings } from './mappings';
 import { coreFields, hotDrinkFields, coldDrinkFields, submitFields, initialCoreValues } from './field-schemas/fieldSchemas';
 import useForm from './useFormHook';
 
-function SimpleForm() {
-    const [ message, setMessage ] = useState('No drink selected');
+export const SimpleForm = () => {
+  const [ message, setMessage ] = useState('No drink selected');
+  const submitAction = () => {
+    setMessage(`Preparing ${inputs['drink']} for ${inputs['employeeName']}`);
+  };
+  const {inputs, handleInputChange, handleSubmit} = useForm(initialCoreValues, submitAction);
+  const isHot = Boolean(inputs['drink'] === 'tea' || inputs['drink'] === 'coffee');
+  const isCold = Boolean(inputs['drink'] === 'soda' || inputs['drink'] === 'water');
 
-    const isHot = () => !!(inputs['drink'] === 'tea' || inputs['drink'] === 'coffee');
-    const isCold = () => !!(inputs['drink'] === 'soda' || inputs['drink'] === 'water');
-
-    const submitAction = () => {
-        setMessage(`Preparing ${inputs['drink']} for ${inputs['employeeName']}`);
-    };
-
-    const {inputs, handleInputChange, handleSubmit} = useForm(initialCoreValues, submitAction);
-
-    return (
-        <Fragment>
-            <form className="Form" onSubmit={handleSubmit} >
-                <DynamicFieldBuilder fields={coreFields} mappings={mappings} onChange={handleInputChange}/>
-                { isHot() ? <DynamicFieldBuilder fields={hotDrinkFields} mappings={mappings} /> : <Fragment /> }
-                { isCold() ? <DynamicFieldBuilder fields={coldDrinkFields} mappings={mappings} /> : <Fragment /> }
-                <DynamicFieldBuilder fields={submitFields} mappings={mappings} />
-            </form>
-            <p>{message}</p>
-        </Fragment>
-    );
-}
-
-export default SimpleForm;
+  return (
+    <Fragment>
+      <form className="Form" onSubmit={handleSubmit} >
+        <DynamicFieldBuilder fields={coreFields} mappings={mappings} onChange={handleInputChange}/>
+        { isHot ? <DynamicFieldBuilder fields={hotDrinkFields} mappings={mappings} /> : <Fragment /> }
+        { isCold ? <DynamicFieldBuilder fields={coldDrinkFields} mappings={mappings} /> : <Fragment /> }
+        <DynamicFieldBuilder fields={submitFields} mappings={mappings} />
+      </form>
+      <p>{message}</p>
+    </Fragment>
+  );
+};
